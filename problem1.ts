3872286e5febd65c50a9eedca9645abda2847e33
fadilarazhya <fadilarazhya03@gmail.com>
2023-05-14 22:46:17 +0800

    add problem1

---
+++ problem1.ts
@@ -0,0 +1,22 @@
+function pairSum(arr: number[], target: number): [number, number] | null {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return null;
+}
+
+console.log(pairSum([1, 2, 3, 4, 6], 6)) 
+console.log(pairSum([2, 5, 9, 11], 11)) 
+console.log(pairSum([1, 3, 5, 7], 12))   
+console.log(pairSum([1, 4, 6, 8], 10))   
+console.log(pairSum([1, 5, 6, 7], 6))
